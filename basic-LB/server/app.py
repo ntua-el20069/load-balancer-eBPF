52fbc8023db076f5ff79d233c4191714c7c02145
ntua-el20069 <el20069@mail.ntua.gr>
2025-11-30 09:24:38 +0200

    [basic-LB] small change to return 500 status some times

--- basic-LB/server/app.py
+++ basic-LB/server/app.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os
 
 app = FastAPI()
 
 
 @app.get("/", tags=["Root"])  # type: ignore
-async def read_root() -> dict:
+async def read_root(action: str | None = None) -> dict:
     num = int(os.getenv("NUM", "0"))
+    # get param from link
+    # lets say that I get a request like http://server:8000/?action=down
+    if action == "down":
+        raise HTTPException(status_code=500, detail=f"server {num} down due to maintenance")
     return {"message": f"welcome from server {num}"}
 
